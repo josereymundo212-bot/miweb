f2d2ecdf4c8e1b88acb667826bf003a89bcc1991
Josevenega <josereymundo212@gmal.com>
2026-06-24 23:33:29 -0400

    fix web

--- web/views.py
+++ web/views.py
@@ -1,16 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    mensaje = ""
-
-    if request.method == "POST":
-        accion = request.POST.get("accion")
-        nombre = request.POST.get("nombre")
-
-        if accion == "saludar" and nombre:
-            mensaje = f"Hola, {nombre}"
-
-        elif accion == "limpiar":
-            mensaje = ""
-
-    return render(request, "web/home.html", {"mensaje": mensaje})
+    return HttpResponse("FUNCIONA RENDER")
